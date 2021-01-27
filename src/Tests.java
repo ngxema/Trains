2e485a14783a381fd2d42d94d157c5d8bd6bb871
ngxema <51192340+ngxema@users.noreply.github.com>
2021-01-27 18:24:02 +0100

    Update Tests.java

--- src/Tests.java
+++ src/Tests.java
@@ -1,9 +1,37 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class Tests {
-  @Test
-  void addition() {
-    assertEquals("NO SUCH ROUTE", Train.calculateDistance("AED"));
-  }
+public class Tests {
+
+    @Before
+    public void prepareRoutes() {
+
+        Train.getRoutes();
+    }
+
+    @Test
+    public void testRoutes() {
+
+        String distance = Train.calculateDistance("A-B-C");
+        assertEquals("9", distance);
+        System.out.println(distance);
+
+        distance = Train.calculateDistance("A-D");
+        assertEquals("5", distance);
+        System.out.println(distance);
+
+        distance = Train.calculateDistance("A-D-C");
+        assertEquals("13", distance);
+        System.out.println(distance);
+
+        distance = Train.calculateDistance("A-E-B-C-D");
+        assertEquals("22", distance);
+        System.out.println(distance);
+
+        distance = Train.calculateDistance("A-E-D");
+        assertEquals("NO SUCH ROUTE", distance);
+        System.out.println(distance);
+    }
+
 }
