e242edb5ed0a9cd9e3c9788d930e97ba73ecd5da
ngxema <51192340+ngxema@users.noreply.github.com>
2021-01-27 18:24:45 +0100

    Update Route.java

--- src/Route.java
+++ src/Route.java
@@ -1,53 +1,29 @@
-import java.util.Objects;
+
 
 public class Route {
 
-  private String route;
-  private int distance;
+    private String route;
+    private int distance;
 
-  Route(String route, int distance) {
-    this.route = route;
-    this.distance = distance;
-  }
-
-  String getRoute() {
-    return route;
-  }
-
-  int getDistance() {
-    return distance;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+    Route(String route, int distance) {
+        this.route = route;
+        this.distance = distance;
     }
 
-    Route route1 = (Route) o;
-
-    if (distance != route1.distance) {
-      return false;
+    String getRoute() {
+        return route;
     }
-    return Objects.equals(route, route1.route);
 
-  }
+    int getDistance() {
+        return distance;
+    }
+    
 
-  @Override
-  public int hashCode() {
-    int result = route != null ? route.hashCode() : 0;
-    result = 31 * result + distance;
-    return result;
-  }
-
-  @Override
-  public String toString() {
-    return "Route{" +
-        "route='" + route + '\'' +
-        ", distance=" + distance +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "Route{" +
+                "route='" + route + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
 }
